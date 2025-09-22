a3f2e4d51d4fb945d257254a4291fd071be090e7
JahnaviGS-153 <117835690+JahnaviGS-153@users.noreply.github.com>
2025-09-22 13:15:07 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,24 @@
+#First comment
+print("Hello World!") #printing a message
+#printing day
+print("Today is Wednesday!")
+
+a=10
+b=20
+print(a,b)
+print(a,end=" ")
+print(b)
+
+c=a+b
+print(a+b)
+print(c)
+print(type(a))
+
+d=-100.2
+print(type(d))
+
+e=4+1j
+print(type(e))
+print(a,d,e)
+print(type(a),type(d),type(e),sep=", ")
+
